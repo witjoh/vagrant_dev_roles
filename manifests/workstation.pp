cec3fb39d09f83e6a83d6cb243a4d4dd4d1f8898
Johan De Wit <johan@koewacht.net>
2017-09-18 11:41:43 +0200

    adds vagrant

--- manifests/workstation.pp
+++ manifests/workstation.pp
@@ -3,6 +3,7 @@ class roles::workstation {
   include profiles::dev_env
   include profiles::packages
   include profiles::ruby
+  include profiles::vagrant
 
 
   Class['profiles::packages'] -> Class['profiles::ruby']
