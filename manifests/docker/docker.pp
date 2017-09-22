06c7098506844973bb286edea957435fed7b56bd
Johan De Wit <johan@koewacht.net>
2017-09-22 10:08:01 +0200

    clean up code

--- manifests/docker/docker.pp
+++ manifests/docker/docker.pp
@@ -2,5 +2,8 @@ class roles::docker::docker {
 
   include profiles::docker::server
   include profiles::docker::registry
+  include profiles::dev_env
+  include profiles::packages
+  include profiles::vagrant
 
 }
