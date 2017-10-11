1733db10052ee4a308fee40b7f62f6abc7cc46e2
Johan De Wit <johan@koewacht.net>
2017-10-11 10:35:57 +0200

    add workstation profile, to enable rspec stuff

--- manifests/docker/docker_vagrant.pp
+++ manifests/docker/docker_vagrant.pp
@@ -1,10 +1,10 @@
 class roles::docker::docker_vagrant {
 
   include profiles::docker::server
-  #include profiles::docker::registry
   include profiles::dev_env
   include profiles::packages
   include profiles::vagrant
   profiles::packer { 'vagrant': }
+  include profiles::workstation
 
 }
