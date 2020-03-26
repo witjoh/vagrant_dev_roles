0cafb0c9a42466e54e567f08eff47bf80a931007
Johan De Wit <johan@koewacht.net>
2020-03-26 16:22:30 +0100

    adds retrospec config

--- manifests/workstation.pp
+++ manifests/workstation.pp
@@ -3,6 +3,7 @@ class roles::workstation {
   include profiles::dev_env
   include profiles::packages
   include profiles::vagrant_rvm
+  include profiles::retrospec
 
   Class['profiles::packages'] -> Class['profiles::vagrant_rvm']
 
